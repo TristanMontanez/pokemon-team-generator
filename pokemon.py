10c70affe8800e6780e772697d9351680223b05a
Tristan Montanez <tristanmiguelmontanez@gmail.com>
2022-08-05 01:51:37 +0800

    Added AJAX to Regenerate

--- pokemon.py
+++ pokemon.py
@@ -18,9 +18,21 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+def get_pokemon_types(pokemon_id):
+    cur = get_db().execute(
+        f'SELECT name FROM type WHERE id IN (SELECT type_id FROM pokemon_type WHERE pokemon_id={pokemon_id});'
+    )
+    pokemon_types = cur.fetchall()
+    cur.close
+
+    for type in pokemon_types:
+        print(type)
+
+    return pokemon_types
+
 def get_pokemon_team():
     cur = get_db().execute(
-        'SELECT * FROM pokemon ORDER BY RANDOM() LIMIT 6'
+        'SELECT id, name, picture FROM pokemon ORDER BY RANDOM() LIMIT 6;'
     )
     pokemon_list = cur.fetchall()
     cur.close()
@@ -28,8 +40,6 @@ def get_pokemon_team():
     pokemon_dict_list = []
     for pokemon in pokemon_list:
         pokemon_dict_list.append(dict(zip(POKEMON_COLUMNS, pokemon)))
-
-    print(pokemon_dict_list)
     return pokemon_dict_list
 
 @app.route('/')
@@ -37,3 +47,10 @@ def hello_world():
     team = get_pokemon_team()
     context = {'team': team}
     return render_template('index.html', **context)
+
+@app.route('/generate')
+def generate():
+    team = get_pokemon_team()
+    for pokemon in team:
+        pokemon['type'] = get_pokemon_types(pokemon['id'])
+    return team
